1a27f2e2e8c2c55611e74be80ee188afbe1997c4
Virendrawebdev <virendra052001@gmail.com>
2026-06-24 14:34:33 +0530

    Typescript fundamental

---
+++ Typescript/day1/index.ts
@@ -0,0 +1,43 @@
+// let a:number=10;
+
+// a=50
+
+// let arr:string[]=["65","52"]
+
+// arr.push("59")
+
+//tuples
+// let arr:[string, string]=["5", "6"]
+// console.log(arr)
+
+//enums
+
+// enum Role{
+//     ADMIN,
+//     USER,
+//     SUP_ADMIN
+// }
+// let user = Role.USER
+// console.log(user)
+
+
+// Any vs unknown vs never
+
+// let a:any="10"
+
+// a="80"
+
+// a=true
+
+// console.log(a)
+
+// union types
+
+let a:string | number |null ="10"
+
+a=90
+
+a=null
+
+console.log(typeof a)
+
